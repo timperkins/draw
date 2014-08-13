2956e6ba06a2af83a8c967ed2ab430c8bef0f6ef
timperkins <tjp503@gmail.com>
2014-08-13 15:52:38 -0600

    cleanup

--- server/server.js
+++ server/server.js
@@ -9,25 +9,7 @@ var config = {
 
 var app = express();
 var server = http.createServer(app);
-
-// app.use(express.logger());
-
-
-// app.use(express.static(__dirname + '/public'));
-// app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-
 var buildFolder = path.resolve(__dirname, '../client/build');
-
-// console.log('bf', buildFolder);
-
-// app.use(express.static(buildFolder));
-// app.use('*.woff', function(req, res, next) {
-// 	res.contentType('font/opentype');
-// 	next();
-// });
-// app.use(function(req, res, next) {
-// 	console.log('use');
-// })
 app.use('/static/', express.static(buildFolder));
 
 app.all('/*', function(req, res) {
@@ -37,9 +19,6 @@ app.all('/*', function(req, res) {
 
 // Start up the server on the port specified in the config
 server.listen(config.listenPort, '0.0.0.0', 511, function() {
-  // // Once the server is listening we automatically open up a browser
-  // var open = require('open');
-  // open('http://localhost:' + config.listenPort + '/');
+  
 });
 console.log('Listening on port: ' + config.listenPort);
-// console.log('static', express.static(__dirname + '/public'));
