e2dda2ed52ceb37aef82b1915b04e362a47a0399
timperkins <tjp503@gmail.com>
2014-08-13 15:54:56 -0600

    cleanup

--- client/src/app.js
+++ client/src/app.js
@@ -1,18 +1,11 @@
-console.log('top of app.js!');
 angular.module('draw', [
-    // 'services.shape',
     'templates-app',
-    // 'ui.router',
     'canvas',
     'full-page',
     'toolbar',
     'header',
     'panel'
   ])
-	// .config(function myAppConfig($urlRouterProvider) {
-	// 	$urlRouterProvider.otherwise('/home');
-	// })
 	.controller('AppController', ['$scope', '$location', function ($scope, $location) {
-		console.log('app controller');
-    $scope.test = 'foo';
+	
 	}]);
